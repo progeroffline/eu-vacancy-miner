a61ee55c2697a1c9819768521819dd68a0217296
progeroffline <progeroffline@gmail.com>
2025-01-31 15:33:38 +0200

    Upload stable version

--- src/settings.py
+++ src/settings.py
@@ -1,22 +1,9 @@
-import configparser
+STOP_WORDS = "Personaldienstleister Personalvermittlung Zeitarbeit Zeitarbeitsfirma Leihfirma Zeitarbeitsunternehmen"
+EUROPA_EU_SERACH_LINKS = [
+    "https://europa.eu/eures/portal/jv-se/advanced-search?page=1&resultsPerPage=50&orderBy=BEST_MATCH&locationCodes=at&positionScheduleCodes=NS,fulltime&sector=i&publicationPeriod=LAST_WEEK&escoIsco=C9&otherBenefitsCodes=1&lang=de",
+    "https://europa.eu/eures/portal/jv-se/advanced-search?page=1&resultsPerPage=50&orderBy=BEST_MATCH&locationCodes=de&positionScheduleCodes=fulltime&sector=i,a&publicationPeriod=LAST_WEEK&escoIsco=C9&lang=de",
+    "https://europa.eu/eures/portal/jv-se/search?page=1&resultsPerPage=10&orderBy=BEST_MATCH&locationCodes=de,at&keywordsEverywhere=W%C3%A4schereimitarbeiter&positionScheduleCodes=fulltime&publicationPeriod=LAST_WEEK&lang=de",
+    "https://europa.eu/eures/portal/jv-se/advanced-search?page=1&resultsPerPage=10&orderBy=BEST_MATCH&locationCodes=be,ee,fi,mt,nl,no&positionScheduleCodes=fulltime&sector=i,a&publicationPeriod=LAST_DAY&escoOccupation=731ecac4-06e8-4ec2-a559-101fecbd9183,1a8bda86-307e-4cc0-a945-ceb678ff1471,23be4cb6-c5e7-449e-8d76-8edfe9a00478,303a1e34-cb16-4054-b323-81e5eec17397&escoIsco=C912,C93,C962,C92,C94&lang=de",
+]
 
-
-config = configparser.ConfigParser()
-config.read("settings.ini")
-
-EUROPELOCATION_CODES = ["de", "at"]
-SEARCH_FILTERS = {
-    "angebotsart": 1,
-    "arbeitszeit": "vz",
-    "beruf": "Agrarwirtschaftlich-technische/r Assistent/in;Fischverarbeiter/in;Flugzeugreiniger/in;Forstwirt/in;Garten-/Landschaftsgestalter/in;Gebäudereiniger/in;Glasreiniger/in;Gärtner/in - Friedhofsgärtnerei;Gärtner/in - Garten- und Landschaftsbau;Gärtner/in - Gemüsebau;Hausdame/Housekeeper;Helfer/in - Forstwirtschaft;Helfer/in - Gartenbau;Helfer/in - Hotel;Helfer/in - Landwirtschaft;Helfer/in - Lebensmittelherstellung;Helfer/in - Reinigung;Helfer/in - Tierpflege;Landwirt/in;Textilreiniger/in;Bügler/in;Greenkeeper-Assistent/in;Spezialreiniger/in",
-    "berufsfeld": "Fischwirtschaft;Forstwirtschaft, Jagdwirtschaft, Landschaftspflege;Gartenbau;Hotellerie;Landwirtschaft;Lebensmittel- und Genussmittelherstellung;Reinigung;Tierpflege",
-    "externestellenboersen": "false",
-    "facetten": "veroeffentlichtseit,arbeitszeit,arbeitsort",
-    "pav": False,
-    "sort": "veroeffdatum",
-    "veroeffentlichtseit": 0,
-    "zeitarbeit": False,
-}
-
-STOP_WORDS = config["Search"]["stop_words"].split()
-TWO_CAPTCHA_API_KEY = config["TwoCaptcha"]["api_key"]
+TWO_CAPTCHA_API_KEY = ""
